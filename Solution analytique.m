cddc51ca9555b301c731847ee6349ab6e5f7d48a
BatCamus <48760816+BatCamus@users.noreply.github.com>
2019-04-03 09:33:13 +0200

    Add files via upload

---
+++ Solution analytique.m
@@ -0,0 +1,21 @@
+%% Solution analytique
+%%%%%%%%%%%%%%%%%%%%%%
+
+%% Constantes
+m1=2; %masse pendule 1
+m2=3; % masse pendule 2
+mu = m1/m2;
+
+l1 = 3; %longueur pendule 1
+l2 = 2; %longueur pendule 2
+
+%% Conditions initiales : 
+theta10 = theta1(t=0);
+theta20 = theta2(t=0);
+theta10p = theta1p(t=0); 
+theta20p = theta2p(t=0);
+
+%% Initialisation 
+theta1= ;% angle pendule 1 avec verticale
+theta2= ;% angle pendule 2 avec verticale 
+
