c1adb765e0d79732241333b52ad8a1e622e494a2
Claire-bd <49227462+Claire-bd@users.noreply.github.com>
2019-05-15 08:32:43 +0200

    Add files via upload

--- calc_Fnl_Double_Pendule.m
+++ calc_Fnl_Double_Pendule.m
@@ -4,8 +4,7 @@ global m1 m2 l1 l2 g mu M
 
 Fnl=zeros(size(X));
 %
-Fnl(1)=(m2*l1*cos(X(1)-X(2))*sin(X(1)-X(2))*(dX(1))^2+m2*l2*sin(X(1)-X(2))*(dX(2))^2+m1*g*sin(X(1))+m2*g*cos(X(1)-X(2))*sin(X(2)))/(m1*l1+m2*l1*sin(X(1)-X(2))^2);
-Fnl(2)=((m1+m2)*l1*sin(X(1)-X(2))*dX(1)^2+m2*l2*cos(X(1)-X(2))*sin(X(1)-X(2))*dX(2)^2+(m1+m2)*g*sin(X(1)-X(2))*cos(X(1)))/(m1*l2+m2*l2*sin(X(1)-X(2))^2);
+Fnl(1)=(m2*l1*cos(X(1)-X(2))*sin(X(1)-X(2))*(dX(1))^2+m2*l2*sin(X(1)-X(2))*(dX(2))^2+m1*g*sin(X(1))+m2*g*cos(X(1)-X(2))*sin(X(2)))/(m1*l1+m2*l1*(sin(X(1)-X(2)))^2);
+Fnl(2)=-((m1+m2)*l1*sin(X(1)-X(2))*dX(1)^2+m2*l2*cos(X(1)-X(2))*sin(X(1)-X(2))*dX(2)^2+(m1+m2)*g*sin(X(1)-X(2))*cos(X(1)))/(m1*l2+m2*l2*(sin(X(1)-X(2)))^2);
 
 end
-
