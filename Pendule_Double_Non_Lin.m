20a6478cf2e2b828ba9ed6f795cacf677c6bdc7c
BatCamus <48760816+BatCamus@users.noreply.github.com>
2019-05-20 14:42:39 +0200

    OUPS

--- Pendule_Double_Non_Lin.m
+++ Pendule_Double_Non_Lin.m
@@ -6,9 +6,7 @@ function dxdt=Pendule_Double_Non_Lin(t,x)
     Fnl=calc_Fnl_Double_Pendule(x);
     dxdt(1)=x(2); 
     dxdt(2)=((-m2*l1*sin(x(1)-x(3))*cos(x(1)-x(3))*x(2)^2)-(m2*l2*sin(x(1)-x(3))*x(4)^2)-(m1*g*sin(x(1)))-(m2*g*sin(x(1)-x(3))*cos(x(3))))/(m1*l1+m2*l1*sin(x(1)-x(3))^2);
-    dxdt(2)=-Fnl(1);
     dxdt(3)= x(4); 
-    dxdt(4)=-Fnl(2);
     dxdt(4)=(((m1+m2)*l1*sin(x(1)-x(3))*x(2)^2)+(m2*l2*sin(x(1)-x(3))*cos(x(1)-x(3))*x(4)^2)+((m1+m2)*g*sin(x(1)-x(3))*cos(x(1))))/(m1*l2+m2*l2*sin(x(1)-x(3))^2);
     
 end
