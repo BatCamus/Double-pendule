3a57cefb7f73ae2227ab3cfc157cd484d7f67b01
RichardGaubert <49227471+RichardGaubert@users.noreply.github.com>
2019-06-07 16:17:53 +0200

    Update Temps_de_retournement_N.m

--- Temps_de_retournement_N.m
+++ Temps_de_retournement_N.m
@@ -8,8 +8,8 @@ precNR=1.e-5;
 % sol init
 Tr=t_init;
 n=1;
-X=X0;dX=dX0; % X et dX sont des matrices (2,1) avec X(1) et X(2) représente les angles theta1 et theta2  
-%% et dX(1) et dX(2) les vitesses associés
+X=X0;dX=dX0; % X et dX sont des matrices (2,1) avec X(1) et X(2) reprÃ©sente les angles theta1 et theta2  
+%% et dX(1) et dX(2) les vitesses associÃ©s
 
 % Fnl=zeros(size(X));
 dFX=zeros(length(X));
@@ -42,13 +42,11 @@ while y2<0 && Tr<w*1e3   %Boucle sur les pas de temps
         iter=iter+1;
     
         if(NUM==0)
-        % Calcul de la Jacobienne calculé à la main
+        % Calcul de la Jacobienne calculÃ© Ã  la main
         [dFX, dFdX]=calc_dFnl_Double_Pendule(X,dX);
-        
-        
         end
         if(NUM==1)
-        % Calcul de la Jacobienne numériquement 
+        % Calcul de la Jacobienne numÃ©riquement 
         [dFX, dFdX]=calc_dFnl_Double_Pendule_numerique(X,dX,Fnl);
         end
         
@@ -59,25 +57,7 @@ while y2<0 && Tr<w*1e3   %Boucle sur les pas de temps
         dX=dX+(2/dt)*deltaX;
         ddX=ddX+(4/dt^2)*deltaX;
         % Calcul du residu
-%         Fnl=calc_Fnl_Double_Pendule(X,dX);
-%         Non appel à la fonction pour gagner du temps
-                ODE=0;
-                Fnl=zeros(size(X));
-                %
-                c=cos(X(1)-X(2));
-                s=sin(X(1)-X(2));
-
-                if(ODE==0)
-                Fnl(1)=(m2*l1*c*s*(dX(1))^2+m2*l2*s*(dX(2))^2+m1*g*sin(X(1))+m2*g*s*cos(X(2)))/(m1*l1+m2*l1*(s)^2);
-                Fnl(2)=-((m1+m2)*l1*s*dX(1)^2+m2*l2*c*s*dX(2)^2+(m1+m2)*g*c*sin(X(1))-(m1+m2)*g*sin(X(2)))/(m1*l2+m2*l2*s^2);
-                end
-
-                % Formule trouvé sur internet
-                if(ODE==1)
-                Fnl(1)=-((-m2*l1*sin(X(1)-X(2))*cos(X(1)-X(2))*dX(1)^2)-(m2*l2*sin(X(1)-X(2))*dX(2)^2)-(m1*g*sin(X(1)))-(m2*g*sin(X(1)-X(2))*cos(X(2))))/(m1*l1+m2*l1*sin(X(1)-X(2))^2);
-                Fnl(2)=-(((m1+m2)*l1*sin(X(1)-X(2))*dX(1)^2)+(m2*l2*sin(X(1)-X(2))*cos(X(1)-X(2))*dX(2)^2)+((m1+m2)*g*sin(X(1)-X(2))*cos(X(1))))/(m1*l2+m2*l2*sin(X(1)-X(2))^2);
-                end
-                
+         Fnl=calc_Fnl_Double_Pendule(X,dX);
         res=P-M*ddX-Fnl;
 %         normres=norm(res)/norm(P);
         normres=norm(deltaX)/norm(X);
